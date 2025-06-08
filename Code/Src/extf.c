2bca10fd15af1cf83511b0c7285dfe8ecee61ccf
cutespider-lzz <3354229011@qq.com>
2025-06-08 16:19:10 +0800

    高度对比测试

--- Code/Src/extf.c
+++ Code/Src/extf.c
@@ -2,12 +2,13 @@
 
 //外接储存模块
 
-uint8_t ExtfSendBuff[1000];//数组长度过短会导致飞控卡死
+uint8_t ExtfSendBuff[2000];//数组长度过短会导致飞控卡死
 
 
 
 void ExtfSend(void)
 {
-	sprintf(ExtfSendBuff,"RollSpeed:%0.2f PitchSpeed:%0.2f HeadingSpeed:%0.2f Roll:%0.2f Pitch:%0.2f Heading:%0.2f Longitude:%0.5f Latitude:%0.5f Height:%0.2f Location_Down:%0.2f Location_East:%0.2f Location_North:%0.2f V_Down:%0.2f V_East:%0.5f V_North:%0.2f \r\n",AHRSData.RollSpeed,AHRSData.PitchSpeed,AHRSData.HeadingSpeed,AHRSData.Roll,AHRSData.Pitch,AHRSData.Heading,Geodetic_Position_data.Longitude,Geodetic_Position_data.Latitude,Geodetic_Position_data.Height,INSGPSData.Location_Down,INSGPSData.Location_East,INSGPSData.Location_North,INSGPSData.Velocity_Down,INSGPSData.Velocity_East,INSGPSData.Velocity_North);
+  sprintf(ExtfSendBuff,"%0.f,%0.f,%0.f,%0.f,%0.f,%0.f,%0.2f,%0.2f,%0.2f,%0.2f,%0.2f,%0.2f,%0.5f,%0.5f,%0.2f,%0.2f,%0.2f,%0.2f,%0.2f,%0.2f,%0.2f,%0.2f,%0.2f%0.5f,%0.5f,%0.2f,%0.2f,%0.2f,%0.2f,%0.f,\r\n",FTimeData.Year,FTimeData.Month,FTimeData.Month_Day,FTimeData.Hour,FTimeData.Minute,FTimeData.Second,AHRSData.RollSpeed,AHRSData.PitchSpeed,AHRSData.HeadingSpeed,AHRSData.Roll,AHRSData.Pitch,AHRSData.Heading,Geodetic_Position_data.Longitude,Geodetic_Position_data.Latitude,Geodetic_Position_data.Height,INSGPSData.Location_Down,INSGPSData.Location_East,INSGPSData.Location_North,INSGPSData.Velocity_Down,INSGPSData.Velocity_East,INSGPSData.Velocity_North,Raw_Sensors_data.Pressure,Raw_Sensors_data.Pressure_Temperature,Raw_GNSS_data.Longitude,Raw_GNSS_data.Latitude,Raw_GNSS_data.Height,Raw_GNSS_data.Velocity_down,Raw_GNSS_data.Velocity_east,Raw_GNSS_data.Velocity_north,Satellites_data.GNSS_satellites);
+	
 	HAL_UART_Transmit_DMA(&huart7,ExtfSendBuff,sizeof(ExtfSendBuff));
 }
--- Code/Src/telemetry.c
+++ Code/Src/telemetry.c
@@ -6,7 +6,7 @@ uint8_t TelemetrySendBuff[100];
 
 void TelemetrySend(void)
 {
-	sprintf(TelemetrySendBuff,"Roll:%0.2f Pitch:%0.2f Heading:%0.2f Longitude:%0.5f Latitude:%0.5f Height:%0.2f Location_Down:%0.2f Location_East:%0.2f Location_North:%0.2f V_Down:%0.2f V_East:%0.5f V_North:%0.2f \r\n",AHRSData.Roll,AHRSData.Pitch,AHRSData.Heading,Geodetic_Position_data.Longitude,Geodetic_Position_data.Latitude,Geodetic_Position_data.Height,INSGPSData.Location_Down,INSGPSData.Location_East,INSGPSData.Location_North,INSGPSData.Velocity_Down,INSGPSData.Velocity_East,INSGPSData.Velocity_North);
+	sprintf(TelemetrySendBuff,"Height:%0.2f,Satellites:%.0f\r\n",Geodetic_Position_data.Height,Satellites_data.GNSS_satellites);
 	HAL_UART_Transmit_DMA(&huart8,TelemetrySendBuff,sizeof(TelemetrySendBuff));
 
 }
